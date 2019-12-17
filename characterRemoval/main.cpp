876411bc767642b7a343fe1f70e17e1c4e558f79
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2019-12-17 20:44:49 +0100

    recursion issues

---
+++ characterRemoval/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+bool isSpace(char c);''
+
+
+int main () {
+
+
+string str{" hello   world     how    are  you       ?"};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
